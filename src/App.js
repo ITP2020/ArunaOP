cd68b66cdee33f5d36a9ae174b6fa01ea29a69d8
chamathnj <chamathjay001@gmail.com>
2020-09-01 16:29:11 +0530

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import BurgerMenu from './animations/BurgerMenu';
 import LeftSideBar from './animations/LeftSideBar';
 import './App.css';
 
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+
+
+// import components chamath
+import InsertProduct from "./components/stock/form/InsertProduct.component";
+// end import
+
+
+
 function App() {
   return (
     <div className="App">
       <LeftSideBar />
+      
+
+      {/* Routes
+      @author chamath */}
+
+      <Router>
+        <Route path="/a" component={InsertProduct}/>
+      </Router>
+      {/* end chamath */}
+
     </div>
   );
 }
---
+++ src/components/stock/form/InsertProduct.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class insertProduct extends Component {
+
+    render() {
+        return (
+
+            <div>
+                <div class="card">
+                    <div class="card-body" style ={{height:" 467px",margin:" 75px", background: "linearGradient(90deg, rgb(181,116,116), white)" ,backgroundColor: "#c32b2b",borderRadius: "40px",borderWidth: "0px",borderColor: "rgb(187,127,127)"}}>
+                        <button class="btn btn-primary" type="button">Button</button>
+                        <textarea></textarea>
+                        <input type="text"/>
+                        </div>
+                </div>
+            </div>
+
+        )
+    }
+}
