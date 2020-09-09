9b9d15e1e8f8d51cc1354b756ed2b95fd918a921
Isuri-Amasha <67119076+Isuri-Amasha@users.noreply.github.com>
2020-09-09 23:18:25 +0530

    third commit of home page

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,9 +9,9 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import Paper from '@material-ui/core/Paper';
+
 import Grid from '@material-ui/core/Grid';
-import { red } from '@material-ui/core/colors';
+
 
 
 const useStyles = makeStyles((theme) => ({
